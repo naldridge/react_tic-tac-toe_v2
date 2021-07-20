d2e7007c1c1b87538ba0513103dd239d97b3596a
Nick Aldridge <naldridge1360@gmail.com>
2021-07-20 15:08:17 -0500

    recreated app with component split

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Board from './components/board';
+import Game from './components/game';
+import './index.css';
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
